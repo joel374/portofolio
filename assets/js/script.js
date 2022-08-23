07d6a9daa87bd6d632b40f27d0bbed928e6e45d6
joel374 <88383646+joel374@users.noreply.github.com>
2022-08-23 13:54:08 +0700

    bebas

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+// event saat link di klik
+$('.page-scroll').on('click', function(e){
+
+	// ambil isi href 
+	var tujuan = $(this).attr('href');
+	// tangkap elemen ybs
+	var elemenTujuan = $(tujuan);
+
+	// pindahkan scroll
+	$('html,body').animate({
+		scrollTop: elemenTujuan.offset().top - 50
+	}, 1100, 'easeInOutExpo');
+
+	e.preventDefault();
+
+});
